295329d4e6d0ddd697c1f89c63f9df08e3ad7c5f
Ivan Babic <ibabic@fesb.hr>
2024-03-17 22:58:46 +0100

    Client - Moving DisplayMessage to common helper

---
+++ Client/Helpers/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Blazor.Popups;
+
+namespace Client.Helpers
+{
+    public static class Helpers
+    {
+        public static async Task<bool> DisplayMessage(bool flag, string message, SfDialogService dialogService)
+        {
+            if (flag)
+            {
+                await dialogService.AlertAsync(message, "Success Operation");
+                return true;
+            }
+            else
+            {
+                await dialogService.AlertAsync(message, "Alert!");
+                return false;
+            }
+        }
+    }
+}
